db7aab3863f50b546c1b35d552233a7bbf2d9029
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-07 15:39:15 +0430

    the logic for connecting to discord written

--- Zenbot/Zenbot/Program.cs
+++ Zenbot/Zenbot/Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Zenbot
 {
-    internal class Program
+    public class Program
     {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                        .AddSingleton(_client)
+                        .AddSingleton(_commands)
+                        .BuildServiceProvider();
+
+            var token = "";
+
+            _client.Log += Log;
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+
+        }
+
+        private async Task RegisterCommandAsync()
+        {
+             _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            var result = await _commands.ExecuteAsync(context, argPos,_services);
+
+            if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+        }
+
+        private Task Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
     }
 }
