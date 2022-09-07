b23ce3e62e8e18775b6e97d68efe65168e0c2855
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-07 16:28:49 +0430

    bot connected to discord

--- Zenbot/Zenbot/Program.cs
+++ Zenbot/Zenbot/Program.cs
@@ -10,66 +10,45 @@ namespace Zenbot
 {
     public class Program
     {
+       
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
         private DiscordSocketClient _client;
-        private CommandService _commands;
-        private IServiceProvider _services;
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+    
 
-        public async Task RunBotAsync()
+        public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
-            _commands = new CommandService();
 
-            _services = new ServiceCollection()
-                        .AddSingleton(_client)
-                        .AddSingleton(_commands)
-                        .BuildServiceProvider();
+            _client.Log += Log;
+            _client.MessageReceived += ClientOnMessageReceived;
 
             var token = "MTAxNTk2NzY0Mjc0MzQ4MDQzMw.GjjLsy.csYNkhHAn0v6arJh6g1mNV9U_l7oD716u55gVc";
 
-            _client.Log += Log;
-
-            await RegisterCommandAsync();
-
             await _client.LoginAsync(TokenType.Bot, token);
-
             await _client.StartAsync();
 
+            // Block this task until the program is closed.
             await Task.Delay(-1);
-
-
         }
 
-        private async Task RegisterCommandAsync()
+        private static Task Log(LogMessage msg)
         {
-             _client.MessageReceived += HandleCommandAsync;
-
-            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-        }
-
-        private async Task HandleCommandAsync(SocketMessage arg)
-        {
-            var message = arg as SocketUserMessage;
-            var context = new SocketCommandContext(_client, message);
-
-            if (message.Author.IsBot) return;
-
-            int argPos = 0;
-
-            var result = await _commands.ExecuteAsync(context, argPos,_services);
-
-            if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
-        }
-
-        private Task Log(LogMessage log)
-        {
-            Console.WriteLine(log.ToString());
+            Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
 
+        private static Task ClientOnMessageReceived(SocketMessage arg)
+        {
+            if (arg.Content.StartsWith("hello!"))
+            {
+                arg.Channel.SendMessageAsync($"Hello {arg.Author.Username}!");
+            }
+           
+            return Task.CompletedTask;
+        }
     }
+
 }
+
