8ab69317502175b989cbcda6dbe607295e30414c
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-09 15:15:50 +0430

    bot commads updated

--- src/Zenbot/Modules/General.cs
+++ src/Zenbot/Modules/General.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,10 +9,20 @@ namespace Zenbot.Modules
 {
     public class General : ModuleBase<SocketCommandContext>
     {
-        [Command("ping")]
-        public async Task PingAsync()
-        {
-            await ReplyAsync("Pong!");
-        }
+
+            [Command("info")]
+            public async Task info(SocketGuildUser socketGuildUser = null)
+            {
+
+                if (socketGuildUser == null)
+                {
+                    socketGuildUser = Context.User as SocketGuildUser;
+                }
+
+                await ReplyAsync($"ID: {socketGuildUser.Id}\n" +
+                    $"Name: {socketGuildUser.Username}#{socketGuildUser.Discriminator}\n" +
+                    $"Created at: {socketGuildUser.CreatedAt}");
+            }
+        
     }
 }
--- src/Zenbot/Program.cs
+++ src/Zenbot/Program.cs
@@ -8,8 +8,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
-using Zenbot.Services;
+
 
 namespace Zenbot
 {
@@ -17,56 +18,74 @@ namespace Zenbot
     /// <summary>
     /// The entry point of the bot.
     /// </summary>
-    class Program
+    public class Program
     {
-      
-        static async Task Main()
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+
+        static void Main(string[] args) => new Program().RunBothAsync().GetAwaiter().GetResult();
+
+        public async Task RunBothAsync()
         {
-            Console.WriteLine("Hello World!");
-            var builder = new HostBuilder()
-                .ConfigureAppConfiguration(x =>
-                {
-                    var configuration = new ConfigurationBuilder()
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("appsettings.json", false, true)
-                        .Build();
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection().AddSingleton(_client)
+                                               .AddSingleton(_commands).BuildServiceProvider();
 
-                    x.AddConfiguration(configuration);
-                })
-                .ConfigureLogging(x =>
-                {
-                    x.AddConsole();
-                    x.SetMinimumLevel(LogLevel.Debug);
-                })
-                .ConfigureDiscordHost<DiscordSocketClient>((context, config) =>
-                {
-                    config.SocketConfig = new DiscordSocketConfig
-                    {
-                        LogLevel = LogSeverity.Debug,
-                        AlwaysDownloadUsers = false,
-                        MessageCacheSize = 200,
-                    };
+            var token = "Tokey Key";
 
-                    config.Token = context.Configuration["Token"];
-                })
-                .UseCommandService((context, config) =>
-                {
-                    config.CaseSensitiveCommands = false;
-                    config.LogLevel = LogSeverity.Debug;
-                    config.DefaultRunMode = RunMode.Sync;
-                })
-                .ConfigureServices((context, services) =>
-                {
-                    services
-                        .AddHostedService<CommandHandler>();
-                })
-                .UseConsoleLifetime();
+            _client.Log += Log;
 
-            var host = builder.Build();
-            using (host)
+
+            await RegisterCommandsAsync();
+
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public async Task RegisterCommandsAsync()
+        {
+            _client.UserJoined += AnnounceJoinedUser;
+            _client.MessageReceived += HandleCommandsAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        public async Task AnnounceJoinedUser(SocketGuildUser user) //Welcomes the new user
+        {
+            var channel = _client.GetChannel(1016646322893377569) as SocketTextChannel; // Gets the channel to send the message in
+            await channel.SendMessageAsync($"Welcome {user.Username} to {channel.Guild.Name}"); //Welcomes the new user
+        }
+
+        private async Task HandleCommandsAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (context != null)
             {
-                await host.RunAsync();
+                if (message.Author.IsBot) return;
+
+                int argPos = 0;
+
+                if (message.HasStringPrefix("!", ref argPos))
+                {
+                    var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                    if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                }
             }
+
+
         }
     }
 }
