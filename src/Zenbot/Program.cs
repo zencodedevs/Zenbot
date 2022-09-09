e1daf43300075cccae29a454973b4c3f4713c392
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-09 09:30:01 +0430

    start point code created to connect the bot

--- src/Zenbot/Program.cs
+++ src/Zenbot/Program.cs
@@ -1,12 +1,78 @@
-﻿using System;
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using Zenbot.BotConnection;
 
 namespace Zenbot
 {
-    public class Program
-    {
-        public static void Main(string[] args) => new Program().connection.MainAsync().GetAwaiter().GetResult();
+    //public class Program
+    //{
+    //    public static void Main(string[] args) => new Program().connection.MainAsync().GetAwaiter().GetResult();
 
-        Connection connection = new Connection();
+    //    Connection connection = new Connection();
+    //}
+
+    /// <summary>
+    /// The entry point of the bot.
+    /// </summary>
+    class Program
+    {
+      
+        static async Task Main()
+        {
+            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", false, true)
+                        .Build();
+
+                    x.AddConfiguration(configuration);
+                })
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                    x.SetMinimumLevel(LogLevel.Debug);
+                })
+                .ConfigureDiscordHost<DiscordSocketClient>((context, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = LogSeverity.Debug,
+                        AlwaysDownloadUsers = false,
+                        MessageCacheSize = 200,
+                    };
+
+                    config.Token = context.Configuration["Token"];
+                })
+                .UseCommandService((context, config) =>
+                {
+                    config.CaseSensitiveCommands = false;
+                    config.LogLevel = LogSeverity.Debug;
+                    config.DefaultRunMode = RunMode.Sync;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    //services
+                    //    .AddHostedService<CommandHandler>();
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
     }
 }
+
