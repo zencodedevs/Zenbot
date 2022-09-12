a2cbfe4b68847103d334e4b06cdde11aab0c64d6
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-12 11:08:51 +0430

    Welcome message made for new user joins server

--- src/Zenbot/Program.cs
+++ src/Zenbot/Program.cs
@@ -18,6 +18,88 @@ namespace Zenbot
     /// <summary>
     /// The entry point of the bot.
     /// </summary>
+    /// 
+    //public class Programa
+    //{
+    //    private DiscordSocketClient _client;
+    //    private CommandService _commands;
+    //    private IServiceProvider _services;
+
+
+    //    static void Main(string[] args) => new Programa().RunBothAsync().GetAwaiter().GetResult();
+
+    //    public async Task RunBothAsync()
+    //    {
+    //        _client = new DiscordSocketClient(new DiscordSocketConfig
+    //        {
+    //            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers
+    //        });
+
+    //        _commands = new CommandService();
+    //        _services = new ServiceCollection().AddSingleton(_client)
+    //                                           .AddSingleton(_commands).BuildServiceProvider();
+
+    //        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+    //        var token = "OTg1O123TAwsNDEwNjk3ODA5.Gikbpe.Mlch69dLKJsfdwwQVasdutyjJdgfsTKus5Ev43456O5gt0j";
+
+    //        _client.Log += Log;
+
+    //        _client.UserJoined += AnnounceJoinedUser;
+
+
+    //        _client.MessageReceived += HandleCommandsAsync;
+
+    //        await _client.LoginAsync(TokenType.Bot, token);
+
+    //        await _client.StartAsync();
+
+    //        await Task.Delay(-1);
+    //    }
+
+    //    private Task Log(LogMessage msg)
+    //    {
+    //        Console.WriteLine(msg.ToString());
+    //        return Task.CompletedTask;
+    //    }
+    //    //public async Task RegisterCommandsAsync()
+    //    //{
+    //    //    _client.UserJoined += AnnounceJoinedUser;
+    //    //    _client.MessageReceived += HandleCommandsAsync;
+    //    //    await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    //    //}
+
+    //    public async Task AnnounceJoinedUser(SocketGuildUser user) //Welcomes the new user
+    //    {
+    //        var channel = _client.GetGuild(941235983284830208).GetChannel(941235983767195659) as SocketTextChannel; // Gets the channel to send the message in
+    //        await channel.SendMessageAsync($"Welcome {user.Username} to {channel.Guild.Name}"); //Welcomes the new user
+    //        return;
+    //    }
+
+
+    //    private async Task HandleCommandsAsync(SocketMessage arg)
+    //    {
+    //        if (arg is SocketUserMessage message)
+    //        {
+    //            //var message = arg as SocketUserMessage;
+    //            var context = new SocketCommandContext(_client, message);
+    //            if (context != null)
+    //            {
+    //                if (message.Author.IsBot) return;
+
+    //                int argPos = 0;
+
+    //                if (message.HasStringPrefix("!", ref argPos))
+    //                {
+    //                    var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+    //                    if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+    //                }
+    //            }
+    //        }
+
+    //    }
+    //}
     public class Program
     {
         private DiscordSocketClient _client;
@@ -29,18 +111,26 @@ namespace Zenbot
 
         public async Task RunBothAsync()
         {
-            _client = new DiscordSocketClient();
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.All | GatewayIntents.GuildMembers
+            });
+
+
             _commands = new CommandService();
             _services = new ServiceCollection().AddSingleton(_client)
                                                .AddSingleton(_commands).BuildServiceProvider();
 
-            var token = "Tokey Key";
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+            var token = "Token Id";
 
             _client.Log += Log;
 
+            _client.UserJoined += AnnounceJoinedUser;
 
-            await RegisterCommandsAsync();
 
+            _client.MessageReceived += HandleCommandsAsync;
 
             await _client.LoginAsync(TokenType.Bot, token);
 
@@ -54,37 +144,35 @@ namespace Zenbot
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
-        public async Task RegisterCommandsAsync()
-        {
-            _client.UserJoined += AnnounceJoinedUser;
-            _client.MessageReceived += HandleCommandsAsync;
-            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-        }
+     
 
         public async Task AnnounceJoinedUser(SocketGuildUser user) //Welcomes the new user
         {
-            var channel = _client.GetChannel(1016646322893377569) as SocketTextChannel; // Gets the channel to send the message in
+            var channel = _client.GetChannel(channelId) as SocketTextChannel; // Gets the channel to send the message in
             await channel.SendMessageAsync($"Welcome {user.Username} to {channel.Guild.Name}"); //Welcomes the new user
+
         }
+
 
         private async Task HandleCommandsAsync(SocketMessage arg)
         {
-            var message = arg as SocketUserMessage;
-            var context = new SocketCommandContext(_client, message);
-            if (context != null)
+            if (arg is SocketUserMessage message)
             {
-                if (message.Author.IsBot) return;
-
-                int argPos = 0;
-
-                if (message.HasStringPrefix("!", ref argPos))
+                var context = new SocketCommandContext(_client, message);
+                if (context != null)
                 {
-                    var result = await _commands.ExecuteAsync(context, argPos, _services);
+                    if (message.Author.IsBot) return;
 
-                    if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                    int argPos = 0;
+
+                    if (message.HasStringPrefix("!", ref argPos))
+                    {
+                        var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                        if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                    }
                 }
             }
-
 
         }
     }
