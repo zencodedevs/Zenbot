0d739e3281768fe3013249b779186cda97ae9e42
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-16 10:43:07 +0430

    public module added for interaction as slash command

---
+++ src/Zenbot/Interactions/Modules/PublicModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+using System.Threading.Tasks;
+namespace Zenbot.Interactions.Modules
+{
+    public class PublicModule : InteractionModuleBase<SocketInteractionContext>
+    {
+
+        [SlashCommand("ping", "Ping the bot")]
+        public async Task ping()
+        {
+            await RespondAsync("done");
+        }
+    }
+}
