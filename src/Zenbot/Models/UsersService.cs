7b15f51bfe3d5d30ae871ea6854586b519d9ce31
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-16 10:54:54 +0430

    User Birthday classes updated

---
+++ src/Zenbot/Models/UsersService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zenbot
+{
+    public class UsersService
+    {
+        ConcurrentBag<BUser> users = new ConcurrentBag<BUser>();
+        public List<BUser> GetUsersBirthday()
+        {
+            return users.Where(a => a.NoticeBrithday).ToList();
+        }
+        public BUser GetUser(ulong Id)
+        {
+            var user = users.FirstOrDefault(a => a.Id == Id);
+            if (user is null)
+            {
+                user = new BUser()
+                {
+                    Id = Id,
+                };
+                users.Add(user);
+            }
+            return user;
+        }
+    }
+}
