8b1559c5546e7b7ca45bda45c365dd08624b5f86
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-16 10:46:44 +0430

    public module for simple command added

---
+++ src/Zenbot/Commands/Modules/PublicModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Zenbot.Commands.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task ping()
+        {
+            int i = 0;
+            int error = (1 / i);
+        }
+    }
+}
