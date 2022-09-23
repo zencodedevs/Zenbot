9d80db7cdedb106146a243c600f4f2c6bd6a352a
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-23 07:40:06 +0430

    HR command added for hr into bot

---
+++ src/BotCore/Interactions/Modules/Moderators/HRModule.cs
@@ -0,0 +1,141 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Zenbot.BotCore;
+using Zenbot.BotCore.Interactions;
+
+namespace BotCore.Interactions.Modules.Moderators
+{
+    [RequireBotPermission(GuildPermission.ManageRoles)]
+    [RequireGuildRole(RequireGuildRole.RoleType.HR)]
+    [Group("hr", "hr commands")]
+    public class HRModule : InteractionModuleBase<CustomSocketInteractionContext>
+    {
+        [Group("users", "users commands")]
+        public class UsersModule : InteractionModuleBase<CustomSocketInteractionContext>
+        {
+            public BotConfiguration BotConfiguration { get; set; }
+            [SlashCommand("send-file", "send file to a user")]
+            public async Task sendFile(IGuildUser user, bool @private, IAttachment file)
+            {
+                await DeferAsync(@private);
+
+                var embed = new EmbedBuilder()
+                {
+                    Title = "New File Received",
+                    Description =
+                    $"**You have new {(file.Ephemeral ? "private " : "")} file from <@{Context.User.Id}>\n**" +
+                    $"Description: {string.Format(BotConfiguration.Text.GreetingMessage, Context.User.Username)}\n" +
+                    $"Size: ` {file.Size.ToSizeSuffix()} `\n" +
+                    $"File Name: ` {file.Filename} `\n" +
+                    $"**[Download The File]({file.Url})**",
+                    ThumbnailUrl = "https://img.icons8.com/fluency/344/double-down.png",
+                    Color = 5814783,
+                };
+
+                var component = new ComponentBuilder()
+                    .WithButton("Download File", null, ButtonStyle.Link, new Emoji("🌐"), file.Url, false, 0);
+
+                await user.SendMessageAsync(embed: embed.Build(), components: component.Build());
+
+                await FollowupAsync("File sent to the user succesfully.", ephemeral: @private);
+            }
+        }
+
+        [Group("roles", "roles commands")]
+        public class RolesModule : InteractionModuleBase<CustomSocketInteractionContext>
+        {
+            //public enum SetType
+            //{
+            //    [ChoiceDisplay("Add Role")]
+            //    Add,
+            //    [ChoiceDisplay("Remove Role")]
+            //    Remove
+            //}
+            //[SlashCommand("set", "add or remove role from user.")]
+            //public async Task add(SetType type, IRole role, IGuildUser user)
+            //{
+            //    if (type == SetType.Add)
+            //    {
+            //        await add(role, user);
+            //    }
+            //    else
+            //    {
+            //        await remove(role, user);
+            //    }
+            //}
+
+            [SlashCommand("add", "add role to user")]
+            public async Task add(IGuildUser user, IRole role)
+            {
+                await DeferAsync();
+                if (user.RoleIds.Contains(role.Id))
+                {
+                    await FollowupAsync("the user have this role already.");
+                    return;
+                }
+                await user.AddRoleAsync(role);
+                await FollowupAsync("the role added to the user succesfuly.");
+            }
+
+            [SlashCommand("remove", "remove role to user")]
+            public async Task remove(IGuildUser user, [Autocomplete()] string role)
+            {
+                await DeferAsync();
+
+                var roleId = ulong.Parse(role);
+
+                if (!user.RoleIds.Contains(roleId))
+                {
+                    await FollowupAsync("the user haven't this role.");
+                    return;
+                }
+
+                await user.RemoveRoleAsync(roleId);
+                await FollowupAsync("the role removed from the user succesfuly.");
+            }
+
+            [AutocompleteCommand("role", "remove")]
+            public async Task user_roles()
+            {
+                var socket = (Context.Interaction as SocketAutocompleteInteraction);
+
+                var results = new List<AutocompleteResult>();
+
+                var targetUser = socket.Data.Options.FirstOrDefault(a => a.Name == "user");
+                if (targetUser != null && targetUser.Value != null)
+                {
+                    bool parseResult = ulong.TryParse(targetUser.Value.ToString(), out ulong userId);
+                    if (parseResult is true)
+                    {
+                        var user = await Context.Guild.GetUserAsync(userId);
+                        foreach (var roleId in user.RoleIds)
+                        {
+                            var role = Context.Guild.GetRole(roleId);
+                            if (role.IsManaged || role.Name == "@everyone") continue;
+
+                            var match = socket.Data.Current.Value.ToString();
+                            if (!role.Name.ToLower().Contains(match.ToLower()))
+                                continue;
+
+                            var result = new AutocompleteResult()
+                            {
+                                Name = role.Name,
+                                Value = role.Id.ToString()
+                            };
+                            results.Add(result);
+                        }
+                    }
+                }
+                await socket.RespondAsync(results);
+            }
+        }
+    }
+}
