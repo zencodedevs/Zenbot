e3f9c90e063e9c44b378a84af82fbe7912229085
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-23 07:36:30 +0430

    two important extention addd into project

---
+++ src/BotCore/Extenstions/LongExtenstions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCore
+{
+    public static class LongExtenstions
+    {
+        public static string ToUserMention(this ulong id)
+        {
+            return $"<@{id}>";
+        }
+    }
+}
