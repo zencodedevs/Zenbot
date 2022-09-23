018dd7d98dcddfef9f4b976b7f4465d503073a86
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-09-23 07:36:03 +0430

    botConfiguratoin changes for more data in config.json

--- src/BotCore/Entities/BotConfiguration.cs
+++ src/BotCore/Entities/BotConfiguration.cs
@@ -12,12 +12,15 @@
     public class BotConfiguration
     {
         public string BotToken { get; set; }
-
-
+        public string Prefix { get; set; }
         public ulong MainGuildId { get; set; }
+
+
         public Channels Channels { get; set; }
         public Roles Roles { get; set; }
-        public string Prefix { get; set; }
+        public Text Text { get; set; }
+        public Files StaticFiles { get; set; }
+
 
         public static BotConfiguration GetConfiguration()
         {
@@ -25,6 +28,14 @@
             var json = JsonConvert.DeserializeObject<BotConfiguration>(data);
             return json;
         }
+    }
+    public class Files
+    {
+        public string GreetingFile { get; set; }
+    }
+    public class Text
+    {
+        public string GreetingMessage { get; set; }
     }
     public class Channels
     {
@@ -35,6 +46,7 @@
     {
         public ulong VarifiedId { get; set; }
         public ulong UnVarifiedId { get; set; }
+        public ulong HR { get; set; }
     }
 
 }
