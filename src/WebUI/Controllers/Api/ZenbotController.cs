0f5889583c1882f58f72b54740cff71850c7ffbc
Reza Saeidinejad <reza.saeidinejad@gmail.com>
2022-09-29 20:18:12 +0330

    implementing layouts (base, auth, error, home and panel layouts)

--- src/WebUI/Controllers/Api/ZenbotController.cs
+++ src/WebUI/Controllers/Api/ZenbotController.cs
@@ -3,23 +3,22 @@ using System.Threading.Tasks;
 using Zen.Mvc;
 using Zenbot.BotCore;
 
-namespace Zenbot.WebUI.Controllers.V1
+namespace Zenbot.WebUI.Controllers.Api
 {
     [Route("api/v{version:apiVersion}/[controller]")]
     public class ZenbotController : ZenController
     {
         private readonly DiscordBotService _botService;
+
         public ZenbotController(DiscordBotService botService)
         {
-            this._botService = botService;
+            _botService = botService;
         }
 
- 
         [HttpGet]
         public async Task GetJiraWebhook(string id)
         {
-            _botService.
+            // statements
         }
-
     }
 }
---
+++ src/WebUI/wwwroot/js/base-home.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    // place home base scripts here...
+});
