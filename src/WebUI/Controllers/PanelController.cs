96879b4eda634dbf3b61689af359d91a2b6534e0
Reza Saeidinejad <reza.saeidinejad@gmail.com>
2022-09-30 16:31:21 +0330

    identity auth pages deleted and panel index initial files created

---
+++ src/WebUI/Controllers/PanelController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zenbot.WebUI.Controllers
+{
+    //[Authorize]
+    public class PanelController : Controller
+    {
+        // GET: /Panel/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
