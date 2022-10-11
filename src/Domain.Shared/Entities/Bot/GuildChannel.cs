d6441a45da55f98d2efaf2df03d4724c3a5bc6c2
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-10-11 15:57:14 +0430

    Welcome & Birthday Messages table created

---
+++ src/Domain.Shared/Entities/Bot/GuildChannel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zen.Domain.Entities.Entity;
+
+namespace Zenbot.Domain.Shared.Entities.Bot
+{
+    public class GuildChannel : Entity
+    {
+        public ulong ChannelId { get; set; }
+        public GuildChannelType Type { get; set; }
+
+        public int GuildId { get; set; }
+        public Guild Guild { get; set; }
+    }
+
+    public enum GuildChannelType
+    {
+        None,
+        Authentication,
+        Logger
+    }
+}
---
+++ src/Infrastructure.Shared/Migrations/20221011112541_mig-add-birthday-welcome-message.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zenbot.Infrastructure.Shared.Migrations
+{
+    public partial class migaddbirthdaywelcomemessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ScrinIOToken",
+                table: "Guilds",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "BirthdayMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    GuildId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BirthdayMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BirthdayMessages_Guilds_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "Guilds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WelcomeMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    GuildId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WelcomeMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WelcomeMessages_Guilds_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "Guilds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BirthdayMessages_GuildId",
+                table: "BirthdayMessages",
+                column: "GuildId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WelcomeMessages_GuildId",
+                table: "WelcomeMessages",
+                column: "GuildId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BirthdayMessages");
+
+            migrationBuilder.DropTable(
+                name: "WelcomeMessages");
+
+            migrationBuilder.DropColumn(
+                name: "ScrinIOToken",
+                table: "Guilds");
+        }
+    }
+}
