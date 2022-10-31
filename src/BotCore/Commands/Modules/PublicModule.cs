b6d1bda8d7da49dc5b9ce65870e807f9d1aaa33f
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-10-31 09:49:24 +0430

    public module updated for help comands

--- src/BotCore/Commands/Modules/PublicModule.cs
+++ src/BotCore/Commands/Modules/PublicModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System.Threading.Tasks;
 
 namespace BotCore.Commands.Modules
@@ -20,11 +21,22 @@ namespace BotCore.Commands.Modules
         [Command("help")]
         public async Task help()
         {
-            await ReplyAsync($"Need help? please consider the following steps! \n\n " +
+            var helpEmbed = new EmbedBuilder()
+                .WithTitle("Help Commands")
+                .WithDescription($"Need help ? please consider the following steps! \n\n " +
                 $"1. `birthday-add` Run this command so you will be registerd in this server and also bot will notify your birthday on server \n" +
-                $"2. `external account` Run this command and register your external account Id like `Jira and Bitbucket` you'll recieve message whenever somone assigns you task you make you a reviewer on a pull request\n" +
+                $"2. `external account` Run this command and register your external account Id like `Jira and Bitbucket` you'll recieve message whenever somone assigns you a task or make you a reviewer on a pull request\n" +
                 $"3. `vocation request` Run this command to request for vocation. You'll recieve message whenever your supervisor accept or decline \n" +
-                $"4. `vocation list` You will get all vocation request history you've done so far");
+                $"4. `vocation list` You will get the list of the requests you made for vocation"
+                ).Build();
+
+            await ReplyAsync(embed: helpEmbed);
+
+            //await ReplyAsync($"Need help? please consider the following steps! \n\n " +
+            //    $"1. `birthday-add` Run this command so you will be registerd in this server and also bot will notify your birthday on server \n" +
+            //    $"2. `external account` Run this command and register your external account Id like `Jira and Bitbucket` you'll recieve message whenever somone assigns you task you make you a reviewer on a pull request\n" +
+            //    $"3. `vocation request` Run this command to request for vocation. You'll recieve message whenever your supervisor accept or decline \n" +
+            //    $"4. `vocation list` You will get all vocation request history you've done so far");
         }
     }
 }
