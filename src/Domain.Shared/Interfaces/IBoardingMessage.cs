1d1fe10cb2a2ce601c72edb3ec3c32b190f6374d
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-11-11 15:35:44 +0430

    Services are created for boarding message

---
+++ src/Domain.Shared/Interfaces/IBoardingMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenbot.Domain.Shared.Entities.Bot;
+using Zenbot.Domain.Shared.Entities.Bot.Dtos;
+
+namespace Zenbot.Domain.Shared.Interfaces
+{
+    public interface IBoardingMessage
+    {
+        Task<BoardingMessage> GetBoardingMessagesByGuildId(int guildId);
+        Task<bool> UpdateBoardingMessage(BoardingMessageDto message);
+    }
+}
