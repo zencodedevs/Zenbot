3bfbc360da16c82a911e85c29b2064c57d1813d9
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-11-15 08:28:52 +0430

    Services implemented for vocation in dashboard

---
+++ src/Domain.Shared/Interfaces/IVocationServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenbot.Domain.Shared.Entities.Bot;
+
+namespace Zenbot.Domain.Shared.Interfaces
+{
+    public interface IVocationServices
+    {
+        Task<List<Vocation>> GetBirthdayMessagesByGuildId(int guildId);
+        //Task<bool> UpdateBirthdayMessage(BirthdayMessageDto message);
+    }
+}
---
+++ src/Infrastructure.Shared/Services/VocatoinServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zen.Domain.Interfaces;
+using Zenbot.Domain.Shared.Entities.Bot;
+using Zenbot.Domain.Shared.Interfaces;
+
+namespace Zenbot.Infrastructure.Shared.Services
+{
+    public class VocatoinServices : IVocationServices
+    {
+        private readonly IEntityFrameworkRepository<Vocation> _repository;
+        public VocatoinServices(IEntityFrameworkRepository<Vocation> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<Vocation>> GetBirthdayMessagesByGuildId(int guildId)
+        {
+            return await _repository.GetListAsync(x => x.GuildId == guildId);
+        }
+    }
+}
