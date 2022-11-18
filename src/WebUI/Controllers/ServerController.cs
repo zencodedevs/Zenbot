b8bcf5a3da27b0ce71baedbe4cc7e58d34d3dc85
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-11-18 17:40:20 +0430

    server views updated

--- src/WebUI/Controllers/ServerController.cs
+++ src/WebUI/Controllers/ServerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Zenbot.Domain.Shared.Interfaces;
@@ -11,11 +12,14 @@ namespace Zenbot.WebUI.Controllers
     {
        
         private readonly IBotUserGuildService _botUserGuildService;
+        private readonly IGuildService _guildService;
 
-        public ServerController(IBotUserGuildService botUserGuildService)
+        public ServerController(IBotUserGuildService botUserGuildService, IGuildService guildService)
         {
             _botUserGuildService = botUserGuildService;
+            _guildService = guildService;
         }
+
 
         [HttpGet]
         public async Task<IActionResult> Index()
@@ -25,5 +29,31 @@ namespace Zenbot.WebUI.Controllers
             var guilds = await _botUserGuildService.GetAllGuildsByUserId((ulong)userid);
             return View(guilds);
         }
+
+        /*
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AuthenticateGuild(int guildId, string password)
+        {
+            await _guildService.UpdatePasswordForGuild(guildId, password);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> GSuiteAuth(int guildId, IFormFile gsuite)
+        {
+            await _guildService.UpdateGSuiteAuthForGuild(guildId, gsuite);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ScrinIOToken(int guildId, string scrinio)
+        {
+            await _guildService.UpdateScrinIOForGuild(guildId, scrinio);
+            return RedirectToAction(nameof(Index));
+        }
+        */
     }
 }
